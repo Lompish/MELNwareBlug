5fa1d76c66c1fb26a7a93191b0b17eb1fe20642a
Malin Hööglund <191246377+MalinHoog@users.noreply.github.com>
2025-10-08 14:29:35 +0200

    cannot get this endpoint to work, I get server error over and over again

---
+++ api/endpointGetForumId.js
@@ -0,0 +1,27 @@
+export default function forumId(app, path, database) {
+  // Hämta forum med id
+  app.get(`/api/forums/by-id/:id`, async (request, response) => {
+    const forumId = request.params.id;
+
+    try {
+      const [forum] = await database.execute(
+        "SELECT * FROM forum WHERE id = ?",
+        [forumId]
+      );
+
+      if (forum.length === 0) {
+        return response.status(404).json({
+          message: "Forum not found."
+        });
+      }
+
+      return response.status(200).json(forum[0]);
+
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({
+        message: "Server error."
+      });
+    }
+  });
+}
