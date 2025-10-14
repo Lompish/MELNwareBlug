3b7f49ac59bde16981a6a1272fd621704a5a8cc7
nadiamessary <nadia.messary.data24sto@edu.tucsweden.se>
2025-10-14 15:19:13 +0200

    moderators can delete posts in their threads

--- api/apiRegister.js
+++ api/apiRegister.js
@@ -25,6 +25,7 @@ import patchPost from "./posts/endpontPatchPost.js";
 // import other from "./endpointOthers.js";
 import getPost from "./posts/endpointGetPost.js";
 import deletePost from "./posts/endpointDeletePost.js";
+import moderatorDeletePost from "./posts/endpointModeratorDeletePost.js";
 //////import { readFileSync } from "fs"
 
 //////const accessList = JSON.parse(
@@ -60,4 +61,5 @@ export default function (server, database) {
   getPost(server, path, database)
   patchPost(server, path, database)
   deletePost(server, path, database)
+  moderatorDeletePost(s)
 }
